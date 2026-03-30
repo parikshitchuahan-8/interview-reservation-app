d5dc87f8c6ebfbbf5f97f82ac1b3c6f8c88710d9
Parikshit Chauhan <parikshitchauhan12345@gmail.com>
2026-03-30 21:22:47 +0530

    Delete system/src/main directory

--- system/src/main/java/com/example/system/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.system.repository;
-
-import com.example.system.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
